7c8bba4e41377398a26fc5d6e020a0b3860da9cc
Jesse Sites <85454439+jesse318s@users.noreply.github.com>
2022-08-31 16:22:05 -0500

    Add files via upload

---
+++ JesseSites4C/JesseSites4C/frmMain.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace JesseSites4C
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.tbxNameInput = new System.Windows.Forms.TextBox();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(30, 68);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(87, 15);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Enter the name";
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(30, 18);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(233, 30);
+            this.lblInstructions.TabIndex = 1;
+            this.lblInstructions.Text = "Enter a name and click the Write button to \r\nadd the name to the text file.";
+            // 
+            // tbxNameInput
+            // 
+            this.tbxNameInput.Location = new System.Drawing.Point(163, 65);
+            this.tbxNameInput.Name = "tbxNameInput";
+            this.tbxNameInput.Size = new System.Drawing.Size(100, 23);
+            this.tbxNameInput.TabIndex = 2;
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(42, 103);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 3;
+            this.btnWrite.Text = "&Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(188, 103);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 142);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.tbxNameInput);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.lblName);
+            this.Name = "frmMain";
+            this.Text = "Name Writer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.TextBox tbxNameInput;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
